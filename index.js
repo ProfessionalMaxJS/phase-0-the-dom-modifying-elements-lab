8b9af71ee6e48ba37ff076a2f9f9939802b41c4d
Max <max.edison@yahoo.com>
2021-09-19 21:40:05 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+main.remove();
+//no longer has DOM node 'main#main'
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "MAX is the champion";
+//has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+
+document.head.append(newHeader);
